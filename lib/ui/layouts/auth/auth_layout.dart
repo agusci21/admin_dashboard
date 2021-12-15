29396714ae00da15ffd61b09182eeb374b1658cb
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-15 13:03:57 -0300

    VIew inicial

--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -5,9 +5,43 @@ class AuthLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AuthLayout'),
+    return Scaffold(
+        body: ListView(
+      children: const [
+        //Desktop
+        _DesktopBody()
+        //Mobile
+        //LinksBar
+      ],
+    ));
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.red,
+      child: Row(
+        children: [
+          //TweeterBackground
+          Expanded(
+              child: Container(
+            color: Colors.blue,
+          )),
+
+          Container(
+            width: 600,
+            height: double.infinity,
+            color: Colors.black,
+            //child:, TODO HAcer el View
+          )
+        ],
       ),
     );
   }
