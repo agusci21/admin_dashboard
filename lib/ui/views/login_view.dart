459849b316024a4263b70b7891cbad542ecea928
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-16 12:02:55 -0300

    Inputs de texto

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,10 +7,66 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      child: const Center(
-        child: Text('Login page'),
+      margin: const EdgeInsets.only(top: 100),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 370),
+          child: Form(
+              child: Column(
+            children: [
+              //Email
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: _buildInputDecoration(
+                    hint: 'Ingrese su E-mail',
+                    label: 'E-mail',
+                    icon: Icons.email_outlined),
+                validator: (value) {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //Password
+              TextFormField(
+                obscureText: true,
+                style: const TextStyle(color: Colors.white),
+                decoration: _buildInputDecoration(
+                    hint: '******',
+                    label: 'Contraseña',
+                    icon: Icons.lock_outline_rounded),
+                validator: (value) {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              LinkText(
+                'Nueva Cuenta',
+                onPressed: () {
+                  //TODO ir al registro
+                },
+              )
+            ],
+          )),
+        ),
       ),
     );
+  }
+
+  InputDecoration _buildInputDecoration(
+      {required String hint, required String label, required IconData icon}) {
+    return InputDecoration(
+        hintStyle: const TextStyle(color: Colors.grey),
+        labelStyle: const TextStyle(color: Colors.grey),
+        hintText: hint,
+        labelText: label,
+        prefixIcon: Icon(
+          icon,
+          color: Colors.grey,
+        ),
+        border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white.withOpacity(.3))),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white.withOpacity(.3))));
   }
 }
